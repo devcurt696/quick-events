a3aeef7b60ef61b4622d5d4597fa881b9ae2f96f
curtcurt69 <cecrouse96@yahoo.com>
2023-12-31 04:03:09 -0500

    changed metadata title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'QuickEvents',
+  title: 'Eventify',
   description: 'A platform form for making events quickly and efficiently.',
   icons: {
     icon: '/assets/images/logo.png'
