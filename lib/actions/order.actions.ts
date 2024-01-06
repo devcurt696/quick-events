da4f29b51a9c6e8a5f37ca907e6a6380b8960fdb
curtcurt69 <cecrouse96@yahoo.com>
2024-01-06 16:25:12 -0500

    added stripe webhook to save payment to the database

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,7 +1,10 @@
 'use server'
 import Stripe from "stripe";
-import { CheckoutOrderParams } from "@/types"
+import { CheckoutOrderParams, CreateOrderParams } from "@/types"
 import { redirect } from "next/navigation";
+import { handleError } from "../utils";
+import { connectToDatabase } from "../mongodb/database";
+import Order from "../mongodb/database/models/order.model";
 
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
@@ -34,5 +37,20 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
           redirect(session.url!);
     } catch (error) {
         throw error;
+    }
+}
+
+export const createOrder = async (order: CreateOrderParams) => {
+    try {
+        await connectToDatabase();
+        const newOrder = await Order.create({
+            ...order,
+            event: order.eventId,
+            buyer: order.buyerId
+        });
+
+        return JSON.parse(JSON.stringify(newOrder));
+    } catch (error) {
+        handleError(error)
     }
 }
